40a9cf6de513bd334b05068f34771add2fe3389c
_Kristof_ <krizziewissie@gmail.com>
2025-05-12 19:47:18 +0200

    progress

--- InsanityLib/Behaviors/BlockEntityBehaviors/PermanentBehaviorManager.cs
+++ InsanityLib/Behaviors/BlockEntityBehaviors/PermanentBehaviorManager.cs
@@ -1,13 +1,18 @@
 ﻿using InsanityLib.Contexts;
+using InsanityLib.Util;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 
 namespace InsanityLib.Behaviors.BlockEntityBehaviors
 {
-    public class PermanentBehaviorManager : BlockEntityBehavior, IEnumerable<BlockEntityBehavior> //TODO turn into ICollectionInstead
+    public class PermanentBehaviorManager : BlockEntityBehavior, IEnumerable<BlockEntityBehavior>
     {
         public PermanentBehaviorManager(BlockEntity blockentity) : base(blockentity)
         {
@@ -31,8 +36,58 @@ namespace InsanityLib.Behaviors.BlockEntityBehaviors
 
         public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve)
         {
-            //TODO check for ID's that no longer exist and remove those behaviors
-            //TODO check for ID's that didn't exist yet and add them
+            //TODO this should be injected in defaultFromTreeAttributes
+
+            var permanentBehaviorsTree = tree.GetTreeAttribute("permanent-behaviors");
+            if (permanentBehaviorsTree == null) return;
+
+            // Remove behaviors that no longer exist in the tree
+            var keysToRemove = Behaviors.Keys.Where(key => !permanentBehaviorsTree.HasAttribute(key)).ToList();
+            foreach (var key in keysToRemove)
+            {
+                var behavior = Behaviors[key].Instance;
+                Behaviors.Remove(key);
+                Blockentity.Behaviors.Remove(behavior);
+                //TODO maybe have an interface for PermanentBehavior Events
+            }
+
+            // Add or update behaviors that exist in the tree but not in the dictionary
+            foreach ((var key, var attr) in permanentBehaviorsTree)
+            {
+                if (Behaviors.ContainsKey(key) || attr is not TreeAttribute behaviorTree) continue;
+
+                var name = behaviorTree.GetString("name");
+                var base64Properties = behaviorTree.GetString("properties");
+
+                try
+                {
+                    if (worldAccessForResolve.ClassRegistry.GetBlockEntityBehaviorClass(name) == null)
+                    {
+                        var pos = Blockentity.Pos;
+                        worldAccessForResolve.Api.GetService<ILogger>().Warning(Lang.Get("Failed to add permanent BlockEntityBehavior {0} for {1}", name, pos));
+                        continue;
+                    }
+
+                    var properties = string.IsNullOrEmpty(base64Properties) ?
+                        new JsonObject(new JObject()) :
+                        JsonObject.FromJson(Encoding.UTF8.GetString(Convert.FromBase64String(base64Properties)));
+
+                    var blockEntityBehavior = worldAccessForResolve.ClassRegistry.CreateBlockEntityBehavior(Blockentity, name);
+                    blockEntityBehavior.properties = properties;
+                    Behaviors.Add(key, new PermanentBlockEntityBehaviorContext
+                    {
+                        Name = name,
+                        Base64EncodedProperties = base64Properties,
+                        Instance = blockEntityBehavior
+                    });
+                    Blockentity.Behaviors.Add(blockEntityBehavior);
+                }
+                catch
+                {
+                    var pos = Blockentity.Pos;
+                    worldAccessForResolve.Api.GetService<ILogger>().Warning(Lang.Get("Failed to add permanent BlockEntityBehavior {0} for {1}", name, pos));
+                }
+            }
         }
 
         //TODO add
