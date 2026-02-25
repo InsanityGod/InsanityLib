af2c0d52c84c3be61352ceebd83d580ee9104423
_Kristof_ <krizziewissie@gmail.com>
2026-02-26 00:01:17 +0100

    init tests

---
+++ InsanityLib.Tests/AlternativeLookupTests.cs
@@ -0,0 +1,27 @@
+﻿using InsanityLib.Util.Span;
+using Vintagestory.API.Common;
+
+namespace InsanityLib.Tests
+{
+    public class AlternativeLookupTests
+    {
+        private readonly Dictionary<AssetLocation, string> test = new()
+        {
+            [new AssetLocation("test1", "someasset")] = "Some Asset",
+            [new AssetLocation("test2","anotherasset")] = "Another Asset",
+            [new AssetLocation("test3", "helloworld")] = "Hello World"
+        };
+
+        [Fact]
+        public void AssetLocationSpanLookup()
+        {
+            var toFind = "test2:anotherasset";
+
+            var lookup = test.GetAlternateLookup<AssetLocationSpan>();
+            
+            Assert.True(lookup.TryGetValue(toFind, out var result));
+
+            Assert.Equal("Another Asset", result);
+        }
+    }
+}
